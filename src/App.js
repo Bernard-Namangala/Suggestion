afa1a118ab0516d0549cbba6c870663eae5d4b6b
Bernard-Namangala <bernardnamangala@gmail.com>
2020-10-03 11:45:35 +0200

    initital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import SearchBox from "./components/SearchBox/SearchBox";
+import SuggestionBox from "./components/SuggestionsBox/SuggestionBox";
+import axios from "axios";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [fetchedData, setFetchedData] = useState(false);
+  const [matchedItems, setMatchedItems] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  const SearchBoxOnChange = async (searchValue) => {
+    const value = searchValue;
+    const matched = [];
+
+    if (!fetchedData) {
+      // if we havent fetched any data yet we get it and store in in the state
+      // to avoid getting new data every time user searched
+      const response = await axios.get(
+        "http://www.mocky.io/v2/5ba8efb23100007200c2750c"
+      );
+      setItems(response.data);
+      // set fetchData to true so that when user types text again we do not make
+      // another api call
+      setFetchedData(true);
+    }
+
+    if (value.length > 1) {
+      items.forEach((element) => {
+        if (
+          element.id.indexOf(value) !== -1 ||
+          element.name.indexOf(value) !== -1 ||
+          element.address.indexOf(value) !== -1 ||
+          element.pincode.indexOf(value) !== -1
+        ) {
+          // if any of those properties contains the searched value push the element into the matched array
+          matched.push(element);
+        }
+      });
+    }
+
+    setSearchValue(value);
+    setMatchedItems(matched);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <SearchBox onChange={SearchBoxOnChange} />
+      <SuggestionBox items={matchedItems} searchValue={searchValue} />
     </div>
   );
 }
